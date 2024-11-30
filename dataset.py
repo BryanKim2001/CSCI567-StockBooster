89db6943b79d47c255f9166531d12cdbfe5e1a8b
BryanKim2001 <anaheim901@gmail.com>
2024-11-30 13:08:02 -0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -2,10 +2,11 @@ import torch
 from torch.utils.data import Dataset
 
 class StockDataset(Dataset):
-    def __init__(self, data_dict, split, price_mean, price_std):
+    def __init__(self, data_dict, split, price_mean, price_std, tweet_dict, embedding_dim=768):
         self.data = []
         self.price_mean = price_mean
         self.price_std = price_std
+        self.embedding_dim = embedding_dim
 
         for company, dates in data_dict[split].items():
             sorted_dates = sorted(dates.keys())
@@ -16,6 +17,11 @@ class StockDataset(Dataset):
                     (dates[sorted_dates[j]]["price"] - price_mean) / price_std if j >= 0  else first_price
                     for j in range(i - 3, i)
                 ]
+                tweet_available = 1 if sorted_dates[i] in tweet_dict.get(company, {}) else -1
+                tweet_vector = tweet_dict.get(company, {}).get(
+                    sorted_dates[i], 2
+                )
+                input_vector += [tweet_vector] + [tweet_available]
                 label = 1 if dates[sorted_dates[i]]["price"] >= 0 else 0
                 self.data.append({"input": input_vector, "label": label})
 
