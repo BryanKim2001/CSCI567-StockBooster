96a5e8a35723e5d27a7f85d2be31e96979fdfb0a
BryanKim2001 <anaheim901@gmail.com>
2024-11-30 13:07:28 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import torch
 import numpy as np
+import json
 from dataset import StockDataset
-from utils import create_data_dict, split_data
+from utils import create_price_dict, split_data, create_tweet_dict
 from train import train_model
 from evaluate import evaluate_model
 from torch.utils.data import DataLoader
 
-data_dict = create_data_dict("data/price/raw")
+data_dict = create_price_dict("data/price/raw")
+tweet_dict = {}
+with open('tweet_sentiment_dict.json', 'r') as tf:
+    tweet_dict = json.load(tf)
+print(len(tweet_dict.items()))
+if (tweet_dict is None):
+    tweet_dict = create_tweet_dict("data/raw_tweets/raw")
+
 split_dict = split_data(data_dict, 0.8)
 train_prices = []
 for company, dates in split_dict["train"].items():
@@ -19,14 +27,14 @@ price_std = np.std(train_prices)
 print(price_mean)
 print(price_std)
 
-train_dataset = StockDataset(split_dict, split="train", price_mean=float(price_mean), price_std=float(price_std))
-test_dataset = StockDataset(split_dict, split="test", price_mean=price_mean, price_std=price_std)
+train_dataset = StockDataset(split_dict, split="train", price_mean=float(price_mean), price_std=float(price_std), tweet_dict=tweet_dict)
+test_dataset = StockDataset(split_dict, split="test", price_mean=price_mean, price_std=price_std, tweet_dict=tweet_dict)
 
-train_loader = DataLoader(train_dataset, batch_size=100000, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+train_loader = DataLoader(train_dataset, batch_size=len(train_dataset), shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=100, shuffle=False)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-input_size = 3
+input_size = 5
 model = train_model(train_loader,input_size, device=device, weight_decay=1e-4)
 
 evaluate_model(model, test_loader, device=device)
