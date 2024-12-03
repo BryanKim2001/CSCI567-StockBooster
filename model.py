964adadf412f3fdb934eab0cd189dd877dea3d4b
BryanKim2001 <anaheim901@gmail.com>
2024-12-02 18:30:22 -0800

    Update model.py

--- model.py
+++ model.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 class StockLogisticRegressionModel(nn.Module):
     def __init__(self, input_size):
         super(StockLogisticRegressionModel, self).__init__()
-        self.fc1 = nn.Linear(input_size, 64)
+        self.fc1 = nn.Linear(input_size, 128)
         self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(64, 1)
+        self.fc2 = nn.Linear(128, 1)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
